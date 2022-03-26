747d6f63e44b41689a6c97bde774ed52cada0f36
LakshithaWeerasooriya <102347853+LakshithaWeerasooriya@users.noreply.github.com>
2022-03-26 19:55:42 +0530

    updated

---
+++ test run cw web/page 1/page01.js
@@ -0,0 +1,14 @@
+//writes group members
+
+	var seconds=0;
+	function displayseconds(){
+	        seconds += 1;
+					
+	}
+	setInterval(displayseconds,1000);
+	
+	function redirectpage(){
+	        window.location="C:/Users/Lakshitha%20Naveen/Desktop/test%20run%20cw%20web/page%202/page%202.html";
+	}
+	
+	setTimeout('redirectpage()',4000);
